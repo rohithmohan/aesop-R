5222c0dfa65e0f292aad4844a5a09874258fd7ac
Rohith Mohan <rohithmohan@gmail.com>
2015-01-26 18:39:43 -0800

    Removed junk
    Removed junk from end of paths file

--- source/AESOP_paths.r
+++ source/AESOP_paths.r
@@ -5,6 +5,3 @@ paths <<- list(apbs="/opt/apbs/", # Path to APBS
 		   coulomb="/opt/apbs/tools/bin/") # Path to Coulomb
 		   #Include a path to python (2.6/2.7) if it is not sourced already
 		   #python="/opt/python") # Path to Python
-
-#paths <<- list(apbs="/usr/local/apbs-1.3-ia32/", # Path to APBS
-#	       pdb2pqr="/usr/local/pdb2pqr-1.7/") # Path to PDB2PQR
